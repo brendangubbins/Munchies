15c4784c8a624ae8fc8bc97523e5a5dca4d2e0a9
brendangubbins <brendansg97@gmail.com>
2021-08-07 00:03:17 -0400

    front-end services

--- client/src/services/register.js
+++ client/src/services/register.js
@@ -3,7 +3,7 @@ const baseUrl = 'http://localhost:5000/api/users';
 
 const register = async credentials => {
   const response = await axios.post(baseUrl, credentials);
-  return response.data
+  return response.data;
 };
 
 export default { register };
