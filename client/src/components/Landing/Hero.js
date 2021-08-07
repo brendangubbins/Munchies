d42b6dee510706cf96f2682e384228449fcc977a
Harjit <harjitliyal@gmail.com>
2021-08-07 13:28:06 -0400

    Added backgrounds & added state to selection buttons

--- client/src/components/Landing/Hero.js
+++ client/src/components/Landing/Hero.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Box, Image } from "@chakra-ui/react";
+import munchiesHero from "../../Images/munchiesHero.png";
 
 const Hero = () => {
-  return <div></div>;
+  //Munchies banner on landing page
+  return (
+    <Box>
+      <Image
+        src={munchiesHero}
+        draggable="false"
+        objectFit="cover"
+        boxSize="100%"
+      />
+    </Box>
+  );
 };
 
 export default Hero;
