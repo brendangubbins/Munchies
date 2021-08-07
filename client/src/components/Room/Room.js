480b26d3ece8cc4a0a9e848e1604bd6f2364f1d8
Harjit <harjitliyal@gmail.com>
2021-08-06 22:08:00 -0400

    Did navbar and made foundation for room

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import Selection from "./Selection";
 
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  overflow: auto;
+`;
+
+const LeftAlign = styled.section`
+  display: flex;
+  background-color: #ffffff;
+  color: black;
+  width: 70%;
+  flex-direction: column;
+  border-right: 2px solid lightgray;
+  padding: 2rem;
+`;
+
+const RightAlign = styled.section`
+  display: flex;
+  width: 100%;
+  background-color: #ffffff;
+  color: black;
+  justify-content: center;
+  align-items: center;
+`;
 const Room = () => {
   return (
-    <div>
-
-    </div>
+    <Wrapper>
+      <LeftAlign>This is where the chat will go </LeftAlign>
+      <RightAlign>
+        <Selection />
+      </RightAlign>
+    </Wrapper>
   );
-}
+};
 
 export default Room;
---
+++ client/src/components/Room/Selection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Selection = () => {
+  return <div>This is where the buttons will go</div>;
+};
+
+export default Selection;
