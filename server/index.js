094cc00945ca055b354a7910efed518a24e7acda
auto-kad <abedin.kadir@gmail.com>
2021-08-07 01:01:29 -0400

    Added yelp API backend functionality; make a request to the yelp-get route to get a response from the yelp API

--- server/index.js
+++ server/index.js
@@ -8,12 +8,14 @@ const port = process.env.PORT || 5000;
 
 const userRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const yelpRouter = require("./controllers/yelp");
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
+app.use("/yelp-get", yelpRouter);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
