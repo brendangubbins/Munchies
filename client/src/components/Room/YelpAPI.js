3c0cb882cdc6c9fb820954a56fa49da8772c18fd
auto-kad <abedin.kadir@gmail.com>
2021-08-07 14:31:40 -0400

    Added functionality to call the Yelp API in the backend, from the frontend by making a request to the backend using axios

---
+++ client/src/components/Room/YelpAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const locationToYelp = async (searchTerms) => {
+
+
+  var url = new URL("http://localhost:5000/yelp-get");
+  url.search = new URLSearchParams(searchTerms);
+
+
+  const yelpResponse = await axios.get(url, searchTerms);
+  console.log("Inside the YelpAPI.js file: ", yelpResponse);
+  return yelpResponse.data;
+  
+};
+
+export default locationToYelp;
+//There should be a text box, where the user enters a keyword (like "pizza" or "deli"),
+// click a button, and that keyword gets sent to this function to get all nearby restaurants
+//locationToYelp(yelpParameters);
